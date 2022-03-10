5672e4a97b55dab7d642b4a539892b583f583a01
Marcin <marcin>
2022-03-10 21:38:27 +0100

    array lectures

--- array_sequences/exercises/anagram.py
+++ array_sequences/exercises/anagram.py
@@ -1,5 +1,28 @@
-def anagram(s1, s2):
-    pass
+def anagram(s1: str, s2: str) -> bool:
+    dict_letters = {}
+    s1, s2, = s1.replace(' ', '').lower(), s2.replace(' ', '').lower()
+
+    for pos, item in enumerate(s1):
+        if s1[pos] in dict_letters:
+            dict_letters[s1[pos]] += 1
+        else:
+            dict_letters[s1[pos]] = 1
+
+    for pos, item in enumerate(s2):
+        if s2[pos] in dict_letters and dict_letters[s2[pos]] > 0:
+            dict_letters[s2[pos]] -= 1
+            if dict_letters[s2[pos]] == 0:
+                del dict_letters[s2[pos]]
+        else:
+            return False
+
+    if not bool(dict_letters):
+        return True
+    return False
 
 
-anagram('dog', 'god')
+print(anagram('go go go', 'gggooo'))
+print(anagram('abc', 'cba'))
+print(anagram('hi man', 'hi     man'))
+print(anagram('aabbcc', 'aabbc'))
+print(anagram('123', '1 2'))
--- array_sequences/exercises/pair_sum.py
+++ array_sequences/exercises/pair_sum.py
@@ -1,4 +1,3 @@
-
 def pair_sum(arr: list, k: int) -> int:
     results: list = []
     while len(arr) > 0:
@@ -9,9 +8,3 @@ def pair_sum(arr: list, k: int) -> int:
                 results.append((x, item))
         result = len(results)
         return result
-
-
-
-print(pair_sum([1,9,2,8,3,7,4,6,5,5,13,14,11,13,-1],10))
-print(pair_sum([1,2,3,1],3))
-print(pair_sum([1,3,2,2],4))
